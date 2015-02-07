35fcb6985fc3088a85087449d39714876f464d25
Eric Mrak <mail@ericmrak.info>
2015-02-06 16:43:44 -0800

    Adding squeeze

--- src/tr/tr.rs
+++ src/tr/tr.rs
@@ -1,8 +1,12 @@
 #![allow(unstable)]
 extern crate getopts;
+extern crate taskpipe;
 
-use std::os;
+use std::os::args;
 use std::io::stdio::{stdin, stdout_raw};
+use std::sync::mpsc::{Sender,Receiver};
+use getopts::Matches;
+use std::sync::Arc;
 
 fn main() {
     let opts = &[
@@ -15,7 +19,7 @@ fn main() {
 
     let usage = getopts::usage("tr - [OPTION]... SET1 [SET2]", opts);
 
-    let matches = match getopts::getopts(os::args().tail(), opts) {
+    let matches = match getopts::getopts(args().tail(), opts) {
         Ok(x) => x,
         Err(e) => {
             println!("{}", e);
@@ -25,57 +29,94 @@ fn main() {
         }
     };
 
-    let delete_flag: bool = matches.opt_present("d");
-    //let squeeze_flag: bool = matches.opt_present("s");
-    //let truncate_flag: bool = matches.opt_present("t");
-    let complement_flag: bool = matches.opts_present(&["c".to_string(), "C".to_string()]);
-
-    let set1 = match matches.free.first() {
-        Some(s) => s.clone(),
-        None => {
-            println!("SET1 is required");
-            print!("{}", usage);
-            return;
-        }
-    };
-    //let set2 = match matches.free.get(1) {
-        //Some(s) => Some(s.clone()),
-        //None => None
-    //};
-
-    if delete_flag {
-        delete(set1, complement_flag);
+    if matches.free.len() == 0 {
+        println!("SET1 is required");
+        print!("{}", usage);
     }
+
+    tr(&matches);
 }
 
-fn write_char(c: char) {
-    match stdout_raw().write_char(c) {
-        Err(e) => panic!("{}", e),
-        Ok(_) => return
-    };
-}
+fn tr(matches: &Matches) {
+    let delete_flag: bool = matches.opt_present("d");
+    let squeeze_flag: bool = matches.opt_present("s");
+    let complement_flag: bool = matches.opts_present(&["c".to_string(), "C".to_string()]);
+    //let truncate_flag: bool = matches.opt_present("t");
 
-fn delete(set1: String, complement: bool) {
-    let mut input = stdin();
+    let set1 = Arc::new(match matches.free.first() {
+        Some(s) => s.clone(),
+        None => String::new()
+    });
+    let set2 = Arc::new(match matches.free.get(1) {
+        Some(s) => s.clone(),
+        None => String::new()
+    });
 
-    let in_set1 = |&: c: char| {
-        set1.chars().any(|s| { s == c })
-    };
+    let delete_set = set1.clone();
+    let squeeze_set = if delete_flag { set2.clone() } else { set1.clone() };
 
-    loop {
-        match input.read_char() {
-            Err(_) => break,
-            Ok(c) => {
-                if complement {
-                    if in_set1(c) {
-                        write_char(c);
+    let guard = taskpipe::input(|tx: Sender<char>| {
+        let mut input = stdin();
+
+        loop {
+            match input.read_char() {
+                Err(_) => break,
+                Ok(c) => tx.send(c).unwrap()
+            };
+        };
+    }).pipe(move |rx: Receiver<char>, tx: Sender<char> | {
+        if delete_flag {
+            for c in rx.iter() {
+                if !complement_flag {
+                    if delete_set.chars().any(|s| { s == c }) {
+                        continue;
                     }
                 } else {
-                    if !in_set1(c) {
-                        write_char(c);
+                    if !delete_set.chars().any(|s| { s == c }) {
+                        continue;
                     }
                 }
+
+                tx.send(c).unwrap();
             }
-        };
+        } else {
+            pump(&rx, &tx);
+        }
+    }).pipe(move |rx: Receiver<char>, tx: Sender<char> | {
+        let mut itr = rx.iter();
+
+        if squeeze_flag {
+            let mut last = itr.next().unwrap();
+            tx.send(last).unwrap();
+
+            for c in itr {
+                if c == last && squeeze_set.chars().any(|s| { s == c }) {
+                    continue;
+                }
+                tx.send(c).unwrap();
+                last = c;
+            }
+        } else {
+            pump(&rx, &tx);
+        }
+    }).end(|rx: Receiver<char>| {
+        let mut out = stdout_raw();
+        for c in rx.iter() {
+            match out.write_char(c) {
+                Err(e) => panic!("{}", e),
+                Ok(_) => continue
+            };
+        }
+    });
+
+    match guard.join() {
+        Ok(_) => return,
+        Err(_) => return
     };
+}
+
+fn pump(rx: &Receiver<char>, tx: &Sender<char>) {
+    for x in rx.iter() {
+        tx.send(x).unwrap();
+    }
 }
